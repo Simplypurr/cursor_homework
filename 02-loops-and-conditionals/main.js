a5306843ac003c079a981ad54462aff6d9d2160a
nchkss <nichickevich@gmail.com>
2022-08-20 17:51:14 +0300

    fixed checker loop

--- 02-loops-and-conditionals/main.js
+++ 02-loops-and-conditionals/main.js
@@ -1,12 +1,12 @@
 // user input check
 let N = parseInt(prompt('Input your start range number for calculation..'), 10);
 while (isNaN(N)) {
-    parseInt(prompt('Your input is not valid, try again. Type your start range number..(ex.10)', 10), 10);
+    N = parseInt(prompt('Your input is not valid, try again. Type your start range number..(ex.10)', 10), 10);
 };
 
 let M = parseInt(prompt('Input your end range number for calculation..'), 10);
 while (isNaN(M)) {
-    parseInt(prompt('Your input is not valid, try again. Type your end range number..(ex.20)', 20), 10);
+    M = parseInt(prompt('Your input is not valid, try again. Type your end range number..(ex.20)', 20), 10);
 };
 
 // user even numbers confirmation
