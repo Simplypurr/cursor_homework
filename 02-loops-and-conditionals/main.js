4014c73efdaea3269f5980bb08b4767bfd4bb228
nchkss <nichickevich@gmail.com>
2022-08-23 19:16:21 +0300

    change variable names

--- 02-loops-and-conditionals/main.js
+++ 02-loops-and-conditionals/main.js
@@ -1,12 +1,12 @@
 // user input check
-let N = parseInt(prompt('Input your start range number for calculation..'), 10);
-while (isNaN(N)) {
-    N = parseInt(prompt('Your input is not valid, try again. Type your start range number..(ex.10)', 10), 10);
+let lowBound = parseInt(prompt('Input your start range number for calculation..'), 10);
+while (isNaN(lowBound)) {
+    lowBound = parseInt(prompt('Your input is not valid, try again. Type your start range number..(ex.10)', 10), 10);
 };
 
-let M = parseInt(prompt('Input your end range number for calculation..'), 10);
-while (isNaN(M)) {
-    M = parseInt(prompt('Your input is not valid, try again. Type your end range number..(ex.20)', 20), 10);
+let highBound = parseInt(prompt('Input your end range number for calculation..'), 10);
+while (isNaN(highBound) || highBound <= lowBound) {
+    highBound = parseInt(prompt('Your input is not valid, try again. Type your end range number..(ex.20)', 20), 10);
 };
 
 // user even numbers confirmation
@@ -15,7 +15,7 @@ const evenNumberSetter = confirm('Do you want to skip even numbers in calculatio
 // create loop for calculations
 let sumInRange = 0;
 
-for (let i = N; i <= M; i++) {
+for (let i = lowBound; i <= highBound; i++) {
    if (evenNumberSetter) {
     sumInRange += (i % 2) ? i : 0;
    } else {
@@ -23,4 +23,4 @@ for (let i = N; i <= M; i++) {
    }
 }
 
-document.writeln(`<p>Sum of numbers in range from ${N} to ${M} ${(evenNumberSetter) ? 'excluding':'including'} even numbers is ${sumInRange}</p>`);
+document.writeln(`<p>Sum of numbers in range from ${lowBound} to ${highBound} ${(evenNumberSetter) ? 'excluding':'including'} even numbers is ${sumInRange}</p>`);
