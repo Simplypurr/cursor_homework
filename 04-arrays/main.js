713a6408a2a83401b110e69efa1da95dd06fff4f
nchkss <nichickevich@gmail.com>
2022-08-31 00:20:32 +0300

    rewrite some function wit map

--- 04-arrays/main.js
+++ 04-arrays/main.js
@@ -5,26 +5,15 @@ const marks = [4, 5, 5, 3, 4, 5];
 
 // create a function which makes a pairs nested array
 function getPairs(arr) {
-    const studentsInPairs = [];
-    const maleStudents = [];
-    const femaleStudents = [];
-    // gender divider
-    for (let i = 0; i < arr.length; i++) {
-        (arr[i].endsWith("а") || arr[i].endsWith("я")) ? femaleStudents.push(arr[i]) : maleStudents.push(arr[i]);
-    }
-    // create pairs 
-    for (let i = 0; i < maleStudents.length; i++) {
-        studentsInPairs.push(new Array(maleStudents[i], femaleStudents[i]));
-    }
+    const maleStudents = arr.filter(item => !(item.endsWith("а") || item.endsWith("я")));
+    const femaleStudents = arr.filter(item => item.endsWith("а") || item.endsWith("я"));
+    const studentsInPairs = maleStudents.map((item, index) => Array(`${item} i ${femaleStudents[index]}`));
     return studentsInPairs;
 }
 
 // create a function which determines a topic for students in pairs
 function topicPicker(arr) {
-    const topicsAndPairs = getPairs(students);
-    for (let i = 0; i < topicsAndPairs.length; i++) {
-        topicsAndPairs[i].push(arr[i]);
-    }
+    const topicsAndPairs = studentPairs.flat().map((item, index) => Array(item, arr[index]));
     return topicsAndPairs;
 }
 
@@ -38,23 +27,27 @@ function getGrade(students, marks) {
 }
 
 // create a function which applies a random grade for each pair of students
-function getRandomGrade(arr){
-    const randomGrade = Array.from(topicPicker(themes));
+function getRandomGrade(arr) {
+    const randomGrade = arr.map((item) => Array(item));
+    const minGrade = 2;
+    const maxGrade = 5;
     for (let i = 0; i < arr.length; i++) {
-        randomGrade[i].push(getRandomNumber(1,5))
+        randomGrade[i].push(getRandomNumber(minGrade,maxGrade))
     }
     return randomGrade;
 }
+
 // random function
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const studentsInPairs = getPairs(students);
-const topicsAndPairs = topicPicker(themes, students);
+const studentPairs = getPairs(students);
+const topicsAndPairs = topicPicker(themes);
 const studentsGrade = getGrade(students, marks);
 const randomPairsGrade = getRandomGrade(topicsAndPairs);
-document.writeln(`<ul><li>${studentsInPairs}</li>
+
+document.writeln(`<ul><li>${studentPairs}</li>
                     <li>${topicsAndPairs}</li>
                     <li>${studentsGrade}</li>
                     <li>${randomPairsGrade}</li></ul>`);
