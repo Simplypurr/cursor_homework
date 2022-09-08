ecf6f6959171e6132ef0397c8298a1e496ff718d
nchkss <nichickevich@gmail.com>
2022-09-08 23:46:00 +0300

    fixed issues after Nazarii Osmak review

--- 05-array-iteration/main.js
+++ 05-array-iteration/main.js
@@ -1,55 +1,72 @@
 // create func get Random Array
 function getRandomArray(length, min, max) {
-    return [...Array(length)].map(() => getRandomNumber(min, max));
+    const arr = [...Array(length)]
+    return arr.map(() => getRandomNumber(min, max));
 }
 console.log(getRandomArray(10, 0, 100));
 
 // create func get Mode of numbers (ignores decimal)
 function getMode(...numbers) {
-    const mode = Array.from(numbers)
-                .filter(i => Number.isInteger(i))
-                .reduce((count, current) => (count[current] = count[current] + 1 || 1, count), {});
-    return Object.keys(mode).reduce((prev, curr) => (mode[prev] > mode[curr] ? prev :
-                                                     mode[prev] < mode[curr] ? curr : `Your entered numbers do not have a mode`));
+    const mode = filterIntegers(numbers).reduce((count, current) => {
+        if (!count[current]) {
+            count[current] = 1;
+        } else {
+            count[current] += 1;
+        }
+        return count; 
+    }, {});
+
+    const result = Object.keys(mode).reduce((prev, curr) => {
+        if (mode[prev] > mode[curr]) {
+            return prev;
+        } else if (mode[prev] < mode[curr]) {
+            return curr;
+        } else {
+            return 'Your entered numbers do not have a mode';
+        }
+    })
+    return result;
 }
+
 console.log(getMode(1, 2.2, 2, 2, 2, 3.3, 3, 3, 4, 5));
 
 // create func get average number (ignore decimal)
 function getAverage(...numbers) {
-    const average = Array.from(numbers)
-                    .filter(i => Number.isInteger(i))
-                    .reduce((total,current) => (total + current)) / (numbers.length);
-    return +average.toFixed(2);
+    const total = filterIntegers(numbers).reduce((total,current) => (total + current));
+    return +(total / numbers.length).toFixed(2);
     
 }
 console.log(getAverage(3,2,3,4,5,6));
 
 // create func get median number (ignore decimal)
 function getMedian(...numbers) {
-    const middleOfArr = Math.floor([...numbers].length / 2);
-    const median = Array.from(numbers)
-                    .filter(i => Number.isInteger(i))
-                    .sort((a,b) => a - b)
-                    .reduce((prev, curr, ind, arr) => (arr.length % 2) ? arr[middleOfArr] : (arr[middleOfArr - 1] + arr[middleOfArr]) / 2);
+    const middleOfArr = Math.floor(numbers.length / 2);
+    const median = filterIntegers(numbers).sort((a,b) => a - b).reduce((prev, curr, ind, arr) => {
+        if (arr.length % 2) {
+            return arr[middleOfArr]
+        } else {
+            return (arr[middleOfArr - 1] + arr[middleOfArr]) / 2
+        }
+    });
     return median;   
 }
 console.log(getMedian(1,3,5,2,4));
 
 // create func filter even numbers
 function filterEvenNumbers(...numbers) {
-    return Array.from(numbers).filter(i => (i % 2));
+    return numbers.filter(i => (i % 2));
 }
 console.log(filterEvenNumbers(1,2,3,4,40,55));
 
 // create func count positive numbers
 function countPositiveNumbers(...numbers) {
-    return Array.from(numbers).filter(i => (i > 0)).length;
+    return numbers.filter(i => (i > 0)).length;
 }
 console.log(countPositiveNumbers(1,2,-3,4,-40,-55));
 
 // create func filter dividing by five
 function getDivideByFive(...numbers) {
-    return Array.from(numbers).filter(i => !(i % 5));
+    return numbers.filter(i => !(i % 5));
 }
 console.log(getDivideByFive(1,2,19,123,20,40,55));
 
@@ -70,9 +87,9 @@ console.log(divideWordByThree('performance'));
 
 // get string permutation
 function getCombinationWord(str) {
-    if (str.length > 10) {return 'Function is limited to operate with string which length not exceed 10 symbols'};
-    if (str.length === 0) {return ''};
-    if (str.length === 1) {return str};
+    if (str.length > 10) return 'Function is limited to operate with string which length not exceed 10 symbols';
+    if (str.length === 0) return '';
+    if (str.length === 1) return str;
     
     const result = [];
 
@@ -91,6 +108,13 @@ function getCombinationWord(str) {
 }
 console.log(getCombinationWord('manama'));
 
+// addition function
+    // random number
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+    // filter integer
+function filterIntegers(intArr) {
+    return intArr.filter(Number.isInteger);
 }
